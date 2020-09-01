09ceb44e7aef7f13a9a9ba37ca2fe4d5dd2fac0f
Mrunali Sawant <Mrunali.Sawant@trigyn.com>
2020-09-01 11:54:08 +0530

    Updates

---
+++ precache-manifest.e15f5ff41fc3e0bcc487af9bb0b0f34b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9240f4acd89702548c0433759deab118",
+    "url": "/reactjs-testing-tutorial/index.html"
+  },
+  {
+    "revision": "7f8ccff5d3e0bb2389bf",
+    "url": "/reactjs-testing-tutorial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf6002774519d99d7884",
+    "url": "/reactjs-testing-tutorial/static/js/2.556090fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-testing-tutorial/static/js/2.556090fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8ccff5d3e0bb2389bf",
+    "url": "/reactjs-testing-tutorial/static/js/main.2cb7d762.chunk.js"
+  },
+  {
+    "revision": "d86941f5d56f0afb2849",
+    "url": "/reactjs-testing-tutorial/static/js/runtime-main.2dcbc10e.js"
+  }
+]);
